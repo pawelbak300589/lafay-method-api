ab2344f844305aa01775c1255951b781bfa9c39b
Pawel Bak <PawelBak@staveleyhead.co.uk>
2020-07-27 15:44:41 +0100

    Update server routes to test simple server API calls

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { randomBytes } = require("crypto");
 const cors = require("cors");
 
 const app = express();
@@ -7,6 +8,44 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+const trainings = {};
+
+app.get("/trainings", (req, res) => {
+  res.send(trainings);
+});
+
+app.post("/trainings", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title, exercises } = req.body;
+
+  trainings[id] = {
+    id,
+    title,
+    exercises,
+  };
+
+  res.status(201).send(trainings[id]);
+});
+
+const exercises = {};
+
+app.get("/exercises", (req, res) => {
+  res.send(exercises);
+});
+
+app.post("/exercises", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title, type } = req.body;
+
+  exercises[id] = {
+    id,
+    title,
+    exercises,
+  };
+
+  res.status(201).send(exercises[id]);
 });
 
 app.listen(5000, () => {
